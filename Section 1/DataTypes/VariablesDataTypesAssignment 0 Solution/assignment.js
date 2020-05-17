9f61f2b5bf8b8ccb09fd2589c14024ac3f716fc1
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2020-05-18 01:46:19 +0530

    data types and operators

---
+++ Section 1/DataTypes/VariablesDataTypesAssignment 0 Solution/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 33;
+let result;
+
+result = 18 + userInput;
+
+result = result - 11;
+result = result * 15;
+result = result / 2;
+
+alert(`user Input is ${userInput}`);
+alert(`result is ${result}`);
---
+++ Section 1/DataTypes/dataTypes.js
@@ -0,0 +1,40 @@
+let a = 22;
+let b = 22.22;
+let c = -3;
+let d = "hi i am a text";
+let e = "heyaaa i am a text too";
+let f = `oopsy i am text`;
+let g = `i am a good boy `;
+let h = "i am a 'pretty' good boy";
+let i = 'i am a "damn" good boy';
+let j = `i am a "very" 'good' boy`;
+let k = "i am a 'super' good boy";
+let l = "my marks are : " + a + " out of " + b;
+let m = `my marks are ${a} out of  ${b}`;
+let n = `i am a  string
+with multiple
+lines and                   Indentation
+`;
+
+let o = "i am a  string\nwith multiple\nlines and\t\t\t\t\tIndentation";
+let p = "i am a  string\nwith multiple\nlines and\t\t\t\t\tIndentation";
+let q = true;
+let r = false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(n);
+console.log(o);
+console.log(p);
+console.log(q);
+console.log(r);
